f6da021304b5576a1229e74cce2d6dfed2d91d4f
Aniket Thakur <superblackpearl007@gmail.com>
2013-10-27 12:41:47 +0530

    Modified Main Activity
    Also added layout resource for landscape position. Added colors
    resources for background color.

--- TicTacToe/src/com/example/tictactoe/TicTacToe.java
+++ TicTacToe/src/com/example/tictactoe/TicTacToe.java
@@ -3,13 +3,18 @@ package com.example.tictactoe;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class TicTacToe extends Activity {
+public class TicTacToe extends Activity implements OnClickListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_tic_tac_toe);
+		
+		View continueButton  = findViewById(R.id.continue_button);
+		continueButton.setOnClickListener(this);
 	}
 
 	@Override
@@ -17,6 +22,12 @@ public class TicTacToe extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.tic_tac_toe, menu);
 		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
